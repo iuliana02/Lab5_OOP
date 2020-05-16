0fa33937cf346a4c15f4482fa3cbee8efbec8033
stefana-rosca <61684255+stefana-rosca@users.noreply.github.com>
2020-05-16 17:54:48 +0300

    edits

--- Lab5/Watchlist.h
+++ Lab5/Watchlist.h
@@ -4,7 +4,7 @@
 
 class Watchlist
 {
-	friend class ControllerBenutzer;
+	/*friend class ControllerBenutzer;
 
 private: 
 	vector <Film> watchlist;
@@ -22,6 +22,29 @@ public:
 	void read_wlist();
 	void write_wlist();
 
-	void copyToFile(string initial_file, string old_file);
+	void copyToFile(string initial_file, string old_file);*/
+protected:
+	std::vector<Film> movielist;
+	int current;
+
+public:
+	Watchlist();
+
+	// Adds a movie to the Watchlist.
+	void add(Film& movie);
+
+	// Returns the movie trailer that is currently being watched.
+	Film getCurrentMovie();
+
+	// Starts the watchlist - plays the first movie trailer.
+	void play();
+
+	// Plays the next movie trailer in the watchlist.
+	void next();
+
+	// Checks if the Watchlist is empty.
+	bool isEmpty();
+
+	virtual ~Watchlist() {}
 };
 
